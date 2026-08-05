d6781b998262f7145310ffe9e9976dc33d4ee865
akashnegi62 <akashsing215@gmail.com>
2026-08-05 14:30:50 +0530

    duplicates

---
+++ Remove-Duplicates/code.cpp
@@ -0,0 +1,23 @@
+// Input: nums = [1,1,2]
+// Output: 2, nums = [1,2,_]
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        if (nums.size() == 0)   // If the array is empty, return 0
+            return 0;
+
+        int i = 0;              // Initialize the first pointer
+
+        for (int j = 1; j < nums.size(); j++) { 
+
+            if (nums[i] != nums[j]) {           // If the elements are not equal
+                i++;                            // Increment the first pointer
+                nums[i] = nums[j];              // copy
+            }
+        }
+
+        return i + 1;           // Return the size of the array
+    }
+};
