c144ecb959b07298429d3f29792cb0bac7e1009e
akashnegi62 <akashsing215@gmail.com>
2026-08-31 22:31:36 +0530

    myself

--- myself/code.cpp
+++ myself/code.cpp
@@ -67,4 +67,19 @@ for(j=1; j<arr.size(); j++){
 
 return i+1;
 
+// move zeros
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
+j = 0;
+
+for(int i = 0; i < nums.size(); i++){
+    if(nums[i] != 0){
+        swap(nums[i], nums[j]);
+        j++
+    }
+}
+
+
 
