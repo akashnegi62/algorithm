d8fd271d8bb5790e1f92d4ae6de1b7d540eeb2cb
akashnegi62 <akashsing215@gmail.com>
2026-09-01 23:54:17 +0530

    myself

--- myself/code.cpp
+++ myself/code.cpp
@@ -81,5 +81,33 @@ for(int i = 0; i < nums.size(); i++){
     }
 }
 
+// happy number
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+
+        while (n != 1) {
+            if (seen.count(n))
+                return false;
+
+            seen.insert(n);
+
+            int sum = 0;
+
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            n = sum;
+        }
+
+        return true;
+    }
+};
+
 
 
